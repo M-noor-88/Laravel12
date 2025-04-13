e710cf0903587836152e38b0585f585b12961ed0
M-noor-88 <taknoor45@gmail.com>
2025-04-13 10:32:15 +0300

    Added from branch noor_branch_l12

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,5 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    // This comment added by 'noor_branch_l12' branch 
 }
---
+++ app/Http/Controllers/newControllerl12.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class newControllerl12 extends Controller
+{
+
+    // this controller added by 'noor_branch_l12' branch 
+    public function index() 
+    {
+        return view('index.php');
+    }
+
+
+}
